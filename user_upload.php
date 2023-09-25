60fb5417c41cbd1b03daca4beef96829f056311b
georgehetrelezis <georgehetrelezis@gmail.com>
2023-09-25 14:11:38 +1000

    [Code] Allow script to successfully insert into database before error handling implementation

--- user_upload.php
+++ user_upload.php
@@ -1,5 +1,37 @@
 <?php
+    $file = fopen('users.csv', 'r');
+    $rows = [];
+    while (($row = fgetcsv($file)) !== false) {
+        $rows[] = $row;
+    }
+    fclose($file);
 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mysql";
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    foreach ($rows as $row) {
+        $name = $conn->real_escape_string($row[0]);
+        $surname = $conn->real_escape_string($row[1]);
+        $email = $conn->real_escape_string($row[2]);
+        
+        $sql = $conn->prepare("INSERT INTO users (name, surname, email) VALUES (?, ?, ?)");
+        $sql->bind_param("sss", $name, $surname, $email);
+
+        if ($sql->execute()) {
+            echo "Record inserted successfully" . $row[0] . $row[1] . $row[2];
+        } else {
+            echo "Error: " . $sql->error;
+        }
+
+        $sql->close();
+    }
+    $conn->close();
 ?>
